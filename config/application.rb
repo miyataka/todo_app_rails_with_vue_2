d70950b94be5f6d326c1d66bb00e0674ae718543
Takayuki Miyahara <voyager.3taka28@gmail.com>
2018-08-19 01:55:46 +0900

    customize config for rails g command

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module TodoApp
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.generators do |g|
+        g.helper false
+        #g.test_framework :rspec
+        g.assets false
+    end
   end
 end
